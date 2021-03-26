0bf48a23e51fbc972c7bf8b6e6cf5e79c4613b06
ryo <ryokokeguchi@gmail.com>
2021-03-26 14:34:27 +0900

    完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import defaultDataset from "./dataset"
 import './assets/styles/style.css';
-import {AnswersList, Chats} from "./components";
+import {AnswersList, Chats} from "./components/index";
+import FormDialog from "./components/Forms/FormDialog"
 
 export default class App extends React.Component {
 
@@ -15,6 +16,8 @@ export default class App extends React.Component {
             open: false
         }
         this.selectAnswer = this.selectAnswer.bind(this)
+        this.handleClickOpen = this.handleClickOpen.bind(this)
+        this.handleClose = this.handleClose.bind(this)
     }
 
     displayNextQuestion = (nextQuestionId) => {
@@ -35,6 +38,11 @@ export default class App extends React.Component {
             case(nextQuestionId === 'init'):
                 setTimeout(()=> this.displayNextQuestion(nextQuestionId),500)
                 break;
+
+            case (nextQuestionId === 'contact'):
+                this.handleClickOpen();
+                break;
+
             case(/^https:*/.test(nextQuestionId)):
                 const a = document.createElement('a');
                 a.href = nextQuestionId
@@ -56,6 +64,14 @@ export default class App extends React.Component {
         }
     }
 
+    handleClickOpen = () => {
+        this.setState({open:true});
+    };
+
+    handleClose = () => {
+        this.setState({open:false});
+    };
+
     componentDidMount() {
         const initAnswer = ''
         this.selectAnswer(initAnswer, this.state.currentId)
@@ -75,6 +91,7 @@ export default class App extends React.Component {
                     <div className="c-box">
                         <Chats chats={this.state.chats}/>
                         <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+                        <FormDialog open={this.state.open} handleClose={this.handleClose}/>
                     </div>
                 </section>
             </div>
